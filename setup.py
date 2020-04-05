50ab7f9f3354a4db18043cfec22555f7af2760ec
abhishekram <abhishek8816@gmail.com>
2020-04-05 11:07:16 +0530

    bump version of pyas2lib

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(root, 'README.rst')) as f:
     README = f.read()
 
 install_requires = [
-    'pyas2lib==1.2.2',
+    'pyas2lib==1.3.0',
     'django>=2.1.9',
     'requests'
 ]
