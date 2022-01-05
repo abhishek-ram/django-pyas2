b8460aa54a11a4c52865f826abf554cd3bb3a1fe
Viet Le <vle@liquidx.com>
2022-01-05 13:56:32 -0500

    Replace deprecated ugettext with gettext_lazy

--- pyas2/urls.py
+++ pyas2/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
+from django.urls import include, path
 
 from pyas2 import views
 
 
 urlpatterns = [
+    # path('pyas2/', include('pyas2.urls')),
     path("as2receive/", views.ReceiveAs2Message.as_view(), name="as2-receive"),
     # Add the url again without slash for backwards compatibility
     path("as2receive", views.ReceiveAs2Message.as_view(), name="as2-receive"),
